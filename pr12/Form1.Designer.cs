c5ac73714615e97450408d5593c00b3d71663ce8
Diana111U <161726589+Diana111U@users.noreply.github.com>
2024-11-25 13:36:13 +0300

    Add files via upload

---
+++ pr12/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace pr12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxSale = new System.Windows.Forms.TextBox();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxInCassa = new System.Windows.Forms.TextBox();
+            this.textBoxSale2 = new System.Windows.Forms.TextBox();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.labelInCassa = new System.Windows.Forms.Label();
+            this.labelSale = new System.Windows.Forms.Label();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.textBoxSale);
+            this.groupBox1.Controls.Add(this.numericUpDownCount);
+            this.groupBox1.Controls.Add(this.textBoxPrice);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сведения о товаре";
+            // 
+            // textBoxSale
+            // 
+            this.textBoxSale.Location = new System.Drawing.Point(134, 108);
+            this.textBoxSale.Name = "textBoxSale";
+            this.textBoxSale.Size = new System.Drawing.Size(60, 20);
+            this.textBoxSale.TabIndex = 7;
+            this.textBoxSale.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxSale.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPrice_KeyPress);
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Location = new System.Drawing.Point(134, 82);
+            this.numericUpDownCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDownCount.TabIndex = 6;
+            this.numericUpDownCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCount.ValueChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(134, 53);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(60, 20);
+            this.textBoxPrice.TabIndex = 5;
+            this.textBoxPrice.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPrice_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Скидка %:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Количество:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Цена:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(72, 26);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(122, 20);
+            this.textBoxName.TabIndex = 1;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            this.textBoxName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxName_KeyPress);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 29);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Название:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(192, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Информация о товарах";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Enabled = false;
+            this.buttonAdd.Location = new System.Drawing.Point(12, 388);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(200, 27);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить товар в корзину";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Enabled = false;
+            this.buttonDelete.Location = new System.Drawing.Point(12, 421);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(113, 23);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Очистить корзину";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(131, 421);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox2.Controls.Add(this.textBoxInCassa);
+            this.groupBox2.Controls.Add(this.textBoxSale2);
+            this.groupBox2.Controls.Add(this.textBoxSum);
+            this.groupBox2.Controls.Add(this.labelInCassa);
+            this.groupBox2.Controls.Add(this.labelSale);
+            this.groupBox2.Controls.Add(this.labelSum);
+            this.groupBox2.Location = new System.Drawing.Point(13, 462);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 123);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Итоговые расчёты";
+            // 
+            // textBoxInCassa
+            // 
+            this.textBoxInCassa.Enabled = false;
+            this.textBoxInCassa.Location = new System.Drawing.Point(101, 88);
+            this.textBoxInCassa.Name = "textBoxInCassa";
+            this.textBoxInCassa.Size = new System.Drawing.Size(87, 20);
+            this.textBoxInCassa.TabIndex = 5;
+            // 
+            // textBoxSale2
+            // 
+            this.textBoxSale2.Enabled = false;
+            this.textBoxSale2.Location = new System.Drawing.Point(101, 57);
+            this.textBoxSale2.Name = "textBoxSale2";
+            this.textBoxSale2.Size = new System.Drawing.Size(87, 20);
+            this.textBoxSale2.TabIndex = 4;
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.Enabled = false;
+            this.textBoxSum.Location = new System.Drawing.Point(101, 24);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.Size = new System.Drawing.Size(87, 20);
+            this.textBoxSum.TabIndex = 3;
+            // 
+            // labelInCassa
+            // 
+            this.labelInCassa.AutoSize = true;
+            this.labelInCassa.Location = new System.Drawing.Point(11, 95);
+            this.labelInCassa.Name = "labelInCassa";
+            this.labelInCassa.Size = new System.Drawing.Size(46, 13);
+            this.labelInCassa.TabIndex = 2;
+            this.labelInCassa.Text = "В кассу";
+            // 
+            // labelSale
+            // 
+            this.labelSale.AutoSize = true;
+            this.labelSale.Location = new System.Drawing.Point(11, 60);
+            this.labelSale.Name = "labelSale";
+            this.labelSale.Size = new System.Drawing.Size(80, 13);
+            this.labelSale.TabIndex = 1;
+            this.labelSale.Text = "Сумма скидки";
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Location = new System.Drawing.Point(11, 26);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(88, 13);
+            this.labelSum.TabIndex = 0;
+            this.labelSum.Text = "Сумма за товар";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 214);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 153);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 596);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "чек за товары";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxSale;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelInCassa;
+        private System.Windows.Forms.Label labelSale;
+        private System.Windows.Forms.Label labelSum;
+        private System.Windows.Forms.TextBox textBoxSum;
+        private System.Windows.Forms.TextBox textBoxInCassa;
+        private System.Windows.Forms.TextBox textBoxSale2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ pr12/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            e.Handled = !((c >= 'а' && c <= 'я') || c == 'ё' || c == 8 || c == 32);
+        }
+
+        private void textBoxPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                return;
+            }
+
+            if (Char.IsControl(e.KeyChar))
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            if ((textBoxName.Text.Length > 0) && (textBoxPrice.Text.Length > 0) && (Convert.ToInt32(numericUpDownCount.Text) > 0) && (textBoxSale.Text.Length > 0))
+            {
+                buttonAdd.Enabled = true;
+                buttonDelete.Enabled = true;
+            }
+            else
+            {
+                buttonAdd.Enabled = false;
+                buttonDelete.Enabled = false;
+            }
+                
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
